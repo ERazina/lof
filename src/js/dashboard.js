bea62895b0cf86eab5ae8963806c707d89632f85
Elina Razina <elina.razina@dataart.com>
2020-10-07 20:34:02 +0300

    jquery mobile touch menu

---
+++ src/js/dashboard.js
@@ -0,0 +1,10 @@
+$(function(){
+
+    $(".res-dash-card-menu-icon").on("tap",function(){
+        $('.res-dash-card-menu').show();
+      });
+    $('.res-dash-card-menu-icon').click(function(){
+        $('.res-dash-card-menu').show();
+    });
+
+});
