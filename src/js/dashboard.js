1ecc22b1bec3286facee2b68fa6b0fefd9cdb64d
Elina Razina <elina.razina@dataart.com>
2020-10-07 20:54:34 +0300

    hide and show menu

--- src/js/dashboard.js
+++ src/js/dashboard.js
@@ -1,10 +1,25 @@
 $(function(){
 
-    $(".res-dash-card-menu-icon").on("tap",function(){
-        $('.res-dash-card-menu').show();
-      });
-    $('.res-dash-card-menu-icon').click(function(){
-        $('.res-dash-card-menu').show();
-    });
 
+      $(".res-dash-card-menu-icon").on("click",function(){
+        if($('.res-dash-card-menu').hasClass('hide')){
+            $('.res-dash-card-menu').addClass('show');
+            $('.res-dash-card-menu').removeClass('hide');
+        }
+        else{
+            $('.res-dash-card-menu').addClass('hide');
+            $('.res-dash-card-menu').removeClass('show');
+        }
+      });
+
+      $(".res-dash-card-menu-icon").on("tap",function(){
+        if($('.res-dash-card-menu').hasClass('hide')){
+            $('.res-dash-card-menu').addClass('show');
+            $('.res-dash-card-menu').removeClass('hide');
+        }
+        else{
+            $('.res-dash-card-menu').addClass('hide');
+            $('.res-dash-card-menu').removeClass('show');
+        }
+      });
 });
