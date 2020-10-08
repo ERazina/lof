d4384777f8f8cf8478ce6206006722b1dcb8ee2f
Elina Razina <elina.razina@dataart.com>
2020-10-08 18:22:57 +0300

    show hide menu

--- src/js/dashboard.js
+++ src/js/dashboard.js
@@ -1,25 +1,13 @@
 $(function(){
-
+// toggle
 
       $(".res-dash-card-menu-icon").on("click",function(){
-        if($('.res-dash-card-menu').hasClass('hide')){
-            $('.res-dash-card-menu').addClass('show');
-            $('.res-dash-card-menu').removeClass('hide');
-        }
-        else{
-            $('.res-dash-card-menu').addClass('hide');
-            $('.res-dash-card-menu').removeClass('show');
-        }
+        $(this).parent().find('.res-dash-card-menu').toggleClass('show');
       });
 
-      $(".res-dash-card-menu-icon").on("tap",function(){
-        if($('.res-dash-card-menu').hasClass('hide')){
-            $('.res-dash-card-menu').addClass('show');
-            $('.res-dash-card-menu').removeClass('hide');
-        }
-        else{
-            $('.res-dash-card-menu').addClass('hide');
-            $('.res-dash-card-menu').removeClass('show');
-        }
-      });
+
+
+      // $(".res-dash-card-menu-icon").on("tap",function(){
+      //   $(this).parent().find('.res-dash-card-menu').toggleClass('show');
+      // })
 });
