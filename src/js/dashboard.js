6219ec3d818c4baa44a4ff6d30870ea36b6a01be
Elina Razina <elina.razina@dataart.com>
2020-10-08 22:51:01 +0300

    for demo and popup page

--- src/js/dashboard.js
+++ src/js/dashboard.js
@@ -1,13 +1,28 @@
 $(function(){
-// toggle
 
-      $(".res-dash-card-menu-icon").on("click",function(){
-        $(this).parent().find('.res-dash-card-menu').toggleClass('show');
+      $(".res-dash-card-menu-icon").not('.res-dash-card').on("click",function(){
+        $(this).parent().find('.res-dash-card-menu').toggleClass('show-menu');
+      });
+
+      $(".res-dash-card-menu-icon").on("tap",function(){
+        $(this).parent().find('.res-dash-card-menu').toggleClass('show-menu');
       });
 
 
+      $(".res-dash-card").not(".res-dash-card-menu-icon").on("click",function(){
+        $('.res-dash-menu-card').toggleClass('show');
+      });
 
-      // $(".res-dash-card-menu-icon").on("tap",function(){
-      //   $(this).parent().find('.res-dash-card-menu').toggleClass('show');
-      // })
+      $(".res-dash-popup-close").on("click",function(){
+        $('.res-dash-menu-card').toggleClass('show');
+      });
+
+      // $(".res-dash-card").not(".res-dash-card-menu-icon").on("click",function(){
+      //   $('.res-dash-menu-card').toggleClass('show');
+      // });
+
+      // $(".res-dash-popup-close").on("tap",function(){
+      //   console.log(1);
+      //   $('.res-dash-menu-card').toggleClass('show');
+      // });
 });
