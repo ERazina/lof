ec915910fabcfef3907e001b152df46f007d59fb
Elina Razina <elina.razina@dataart.com>
2020-10-09 17:16:15 +0300

    script for menu and popup and close menu

--- src/js/dashboard.js
+++ src/js/dashboard.js
@@ -1,28 +1,36 @@
 $(function(){
 
-      $(".res-dash-card-menu-icon").not('.res-dash-card').on("click",function(){
-        $(this).parent().find('.res-dash-card-menu').toggleClass('show-menu');
-      });
+        //open popup click
+        $(".res-dash-card").on("click",function(e){
+          e.stopPropagation();
+          $('.res-dash-menu-card').toggleClass('show');
+        });
 
-      $(".res-dash-card-menu-icon").on("tap",function(){
-        $(this).parent().find('.res-dash-card-menu').toggleClass('show-menu');
-      });
+        //open popup tap
+        $(".res-dash-card").on("tap",function(e){
+          e.stopPropagation();
+          $('.res-dash-menu-card').toggleClass('show');
+        });
 
+      //open menu click
+        $(".res-dash-card-menu-icon").not(".res-dash-card").on("click",function(e){
+          e.stopPropagation();
+          $(this).parent().find('.res-dash-card-menu').toggleClass('show-menu');
+        });
 
-      $(".res-dash-card").not(".res-dash-card-menu-icon").on("click",function(){
-        $('.res-dash-menu-card').toggleClass('show');
-      });
+        //open menu tap
+        $(".res-dash-card-menu-icon").not(".res-dash-card").on("tap",function(e){
+          e.stopPropagation();
+          $(this).parent().find('.res-dash-card-menu').toggleClass('show-menu');
+        });
 
+      //close popup click
       $(".res-dash-popup-close").on("click",function(){
         $('.res-dash-menu-card').toggleClass('show');
       });
 
-      // $(".res-dash-card").not(".res-dash-card-menu-icon").on("click",function(){
-      //   $('.res-dash-menu-card').toggleClass('show');
-      // });
-
-      // $(".res-dash-popup-close").on("tap",function(){
-      //   console.log(1);
-      //   $('.res-dash-menu-card').toggleClass('show');
-      // });
+      //close popup tap
+      $(".res-dash-popup-close").on("tap",function(){
+        $('.res-dash-menu-card').toggleClass('show');
+      });
 });
