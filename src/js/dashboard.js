7c01fe6c07fcb8e8b8507aad809e38cbaacdb8c7
Elina Razina <elina.razina@dataart.com>
2020-10-15 17:56:34 +0300

    timedate

--- src/js/dashboard.js
+++ src/js/dashboard.js
@@ -53,6 +53,55 @@ $(function(){
             monthNames: ['Januar', 'Februar', 'März', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'Dezember']
           });
 
+          $.datepicker.setDefaults($.datepicker.regional['de']);
+
+          /* Локализация timepicker */
+      $.timepicker.regional['de'] = {
+        timeOnlyTitle: 'Выберите время',
+        timeText: 'Время',
+        hourText: 'Uhr',
+        minuteText: 'Minute',
+        timezoneText: 'Часовой пояс',
+        timeFormat: 'HH:mm',
+        isRTL: false
+      };
+      $.timepicker.setDefaults($.timepicker.regional['de']);
+
+      var myControl = {
+        create: function(tp_inst, obj, unit, val, min, max, step){
+          $('<input class="ui-timepicker-input" value="'+val+'" style="width:50%">')
+            .appendTo(obj)
+            .spinner({
+              min: min,
+              max: max,
+              step: step,
+              change: function(e,ui){
+                if(e.originalEvent !== undefined)
+                  tp_inst._onTimeChange();
+                tp_inst._onSelectHandler();
+              },
+              spin: function(e,ui){
+                tp_inst.control.value(tp_inst, obj, unit, ui.value);
+                tp_inst._onTimeChange();
+                tp_inst._onSelectHandler();
+              }
+            });
+          return obj;
+        },
+        options: function(tp_inst, obj, unit, opts, val){
+          if(typeof(opts) == 'string' && val !== undefined)
+            return obj.find('.ui-timepicker-input').spinner(opts, val);
+          return obj.find('.ui-timepicker-input').spinner(opts);
+        },
+        value: function(tp_inst, obj, unit, val){
+          if(val !== undefined)
+            return obj.find('.ui-timepicker-input').spinner('value', val);
+          return obj.find('.ui-timepicker-input').spinner('value');
+        }
+      };
+
+      $("#datepicker").datetimepicker({controlType: myControl});
+
 
 
 
